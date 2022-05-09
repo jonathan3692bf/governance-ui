4dc51c0557219e215752bfc4531d96c985c77379
Corinne Bösch <corinne.boesch@gmail.com>
2022-05-10 00:24:31 +0100

    revert Hero bkg mobile / desktop

--- components_2/Hero.tsx
+++ components_2/Hero.tsx
@@ -1,24 +1,25 @@
 import Image from 'next/image'
 import { Section } from 'pages/solana'
-import useIsExtensionWidth from 'components_2/Utils'
+// import useIsExtensionWidth from 'components_2/Utils'
 
 export default function Hero({
   backgroundColor = 'bg-[#282933]',
-  backgroundImgSrcMobile = '/img/realms-web/backgrounds/landing-hero-mobile.png',
-  backgroundImgSrcDesktop = '/img/realms-web/backgrounds/landing-hero-desktop.png',
+  // backgroundImgSrcMobile = '/img/realms-web/backgrounds/landing-hero-mobile.png',
+  backgroundImgSrc = '/img/realms-web/backgrounds/landing-hero-desktop.png',
   height = 'h-[560px] md:h-[600px]',
   children,
 }) {
-  const isExtensionWidth = useIsExtensionWidth({ width: '640' })
+  // const isExtensionWidth = useIsExtensionWidth({ width: '640' })
 
   return (
     <div className={`${backgroundColor}`}>
       <div className={`absolute w-full ${height}`}>
         <Image
           alt="hero image"
-          src={
-            isExtensionWidth ? backgroundImgSrcMobile : backgroundImgSrcDesktop
-          }
+          src={backgroundImgSrc}
+          // src={
+          //   isExtensionWidth ? backgroundImgSrcMobile : backgroundImgSrcDesktop
+          // }
           layout="fill"
           objectFit="cover"
           quality={100}
